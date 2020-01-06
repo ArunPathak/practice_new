94908fc37fa65f4d65b2537116f56b59862ab44c
ArunPathak <arunpathak241994@gmail.com>
2020-01-07 00:26:33 +0530

    Resubmitting assignment

--- guessinggame.sh
+++ guessinggame.sh
@@ -7,17 +7,23 @@ function guess {
 do
 	echo "Guess the no. of files in the current directory:"
 	read choice
-	count=$(ls | wc -l)
-	if [[ $choice -eq $count ]]
+	if [[ "$choice" =~ ^[0-9]+$ ]]
 	then
-		echo "Congratulations... Correct Guess!!!"
-		break
-	elif [[ $choice -gt $count ]]
-	then	
-		echo "Your guess was too high."
+		count=$(ls -1a | wc -l)
+		if [[ $choice -eq $count ]]
+		then
+			echo "Congratulations... Correct Guess!!!"
+			break
+		elif [[ $choice -gt $count ]]
+		then	
+			echo "Your guess was too high."
+		else
+			echo "Your guess was too low."
+		fi
 	else
-		echo "Your guess was too low."
+		echo "Please provide integer."
 	fi
+		
 done	
 
 }
